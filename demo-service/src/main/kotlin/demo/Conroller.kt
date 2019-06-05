c2c5def534dbb9bda735181c4a49cbc7966a24bd
evgzakharov <evgzakharov88@gmail.com>
2019-06-05 11:23:56 +0300

    minor

--- demo-service/src/main/kotlin/demo/Conroller.kt
+++ demo-service/src/main/kotlin/demo/Conroller.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
-import java.util.concurrent.CompletableFuture
 
 @RestController
 class DemoController(
