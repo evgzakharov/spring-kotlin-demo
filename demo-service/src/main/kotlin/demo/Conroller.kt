7bd897cf68db8cbe9385da282fe218332dc9994a
evgzakharov <evgzakharov88@gmail.com>
2019-06-05 14:20:30 +0300

    init webflux start

--- demo-service/src/main/kotlin/demo/Conroller.kt
+++ demo-service/src/main/kotlin/demo/Conroller.kt
@@ -15,36 +15,25 @@ class DemoController(
 ) {
     @PostMapping
     fun processRequest(@RequestBody serviceRequest: Mono<ServiceRequest>): Mono<Response> {
-        val cacheRequest = serviceRequest.cache()
-            .publishOn(Schedulers.parallel())
+//        val authInfo = getAuthInfo(serviceRequest.authToken)
+//
+//        val userInfo = findUser(authInfo.userId)
+//
+//        val cardFromInfo = findCardInfo(serviceRequest.cardFrom)
+//        val cardToInfo = findCardInfo(serviceRequest.cardTo)
+//
+//        sendMoney(cardFromInfo.cardId, cardToInfo.cardId, serviceRequest.amount)
+//
+//        val paymentInfo = getPaymentInfo(cardFromInfo.cardId)
+//
+//        return SuccessResponse(
+//            amount = paymentInfo.currentAmount,
+//            userName = userInfo.name,
+//            userSurname = userInfo.surname,
+//            userAge = userInfo.age
+//        )
 
-        val userInfoMono = cacheRequest.flatMap {
-            getAuthInfo(it.authToken)
-        }.flatMap {
-            findUser(it.userId)
-        }
-
-        val cardFromInfoMono = cacheRequest.flatMap { findCardInfo(it.cardFrom) }
-        val cardToInfoMono = cacheRequest.flatMap { findCardInfo(it.cardTo) }
-
-        val paymentInfoMono = cardFromInfoMono.zipWith(cardToInfoMono)
-            .flatMap { (cardFromInfo, cardToInfo) ->
-                cacheRequest.flatMap { request ->
-                    sendMoney(cardFromInfo.cardId, cardToInfo.cardId, request.amount).map { cardFromInfo }
-                }
-            }.flatMap {
-                getPaymentInfo(it.cardId)
-            }
-
-        return userInfoMono.zipWith(paymentInfoMono)
-            .map { (userInfo, paymentInfo) ->
-                SuccessResponse(
-                    amount = paymentInfo.currentAmount,
-                    userName = userInfo.name,
-                    userSurname = userInfo.surname,
-                    userAge = userInfo.age
-                )
-            }
+        TODO()
     }
 
     private fun getPaymentInfo(cardId: Long): Mono<PaymentTransactionInfo> {
