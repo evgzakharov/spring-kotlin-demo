ddc0820ccaf916a889c5a532baaff5f3c6bb17ef
evgzakharov <evgzakharov88@gmail.com>
2019-06-05 17:34:48 +0300

    add test digit property

--- demo-service/src/main/kotlin/demo/Config.kt
+++ demo-service/src/main/kotlin/demo/Config.kt
@@ -1,10 +1,8 @@
 package demo
 
-import org.jetbrains.annotations.NotNull
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
-import javax.annotation.PostConstruct
+import javax.validation.constraints.Pattern
 
 @ConfigurationProperties(prefix = "url")
 @Validated
@@ -12,5 +10,7 @@ data class DemoConfig(
     val auth: String,
     val card: String,
     val payment: String,
-    val user: String
+    val user: String,
+    @Pattern(regexp = "\\d+")
+    val testDigit: String
 )
