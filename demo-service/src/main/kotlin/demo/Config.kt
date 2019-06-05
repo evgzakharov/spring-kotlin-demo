e5bc2a4641cf9eff1f5ee5744b75760c29b4c7d8
evgzakharov <evgzakharov88@gmail.com>
2019-06-05 12:39:46 +0300

    fix config

--- demo-service/src/main/kotlin/demo/Config.kt
+++ demo-service/src/main/kotlin/demo/Config.kt
@@ -8,16 +8,9 @@ import javax.annotation.PostConstruct
 
 @ConfigurationProperties(prefix = "url")
 @Validated
-class DemoConfig {
-    @NotNull
-    lateinit var auth: String
-
-    @NotNull
-    lateinit var card: String
-
-    @NotNull
-    lateinit var payment: String
-
-    @NotNull
-    lateinit var user: String
-}
+data class DemoConfig(
+    val auth: String,
+    val card: String,
+    val payment: String,
+    val user: String
+)
